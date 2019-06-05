80d816afb0eb0da3be0f42f73fa9ecdb1be4241f
zareen <zareentazshukur@gmail.com>
2019-06-04 21:24:08 -0500

    update the  code for output file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,14 +1,15 @@
 import os
 import csv
+ # path for the file
 pybank = "budget_data.csv"
 totalmonths=[]
 total= []
-#average =0
 average_change=[]
 
-
+# Reading using csv module
 with open(pybank, newline="", encoding="utf-8")as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    csvwriter = csv.writer(csvfile,delimiter=',')
     csv_header = next(csvfile)
     print(f"Header: {csv_header}")
     for row in csvreader:
@@ -34,5 +35,13 @@ print("-------------------------------------------------------")
 print("Total Months: " +str(len(totalmonths)))
 print(f"Total: ${sum(total)}")
 print(f"Average Change: ${round(sum(average_change)/len(average_change),2)}")
-print(f"Greatest Increase in Profits:{totalmonths[Max_month]} ($ {Greatest_profit})")
-print(f"Greatest Decrease in Profits:{totalmonths[Min_month]} ($ {Greatest_loss})")
+print(f"Greatest Increase in Profits:{totalmonths[Max_month]} (${Greatest_profit})")
+print(f"Greatest Decrease in Profits:{totalmonths[Min_month]} (${Greatest_loss})")
+file1=open("output.txt","w")
+file1.write("Financial Analysis \n -----------------------------------------------  ")
+file1.write("\n" +"Total Months: " +str(len(totalmonths))+ "\n")
+file1.write("\n" + f"Total: ${sum(total)}" +"\n")
+file1.write("\n" + f"Average Change: ${round(sum(average_change)/len(average_change),2)}" + "\n" )
+file1.write("\n" + f"Greatest Increase in Profits:{totalmonths[Max_month]} (${Greatest_profit})" + "\n" )
+file1.write("\n" + f"Greatest Decrease in Profits:{totalmonths[Min_month]} (${Greatest_loss})" + "\n" )
+file1.close()
